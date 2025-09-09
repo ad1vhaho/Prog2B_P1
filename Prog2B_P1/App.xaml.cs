1de5a9b2b460a889cbfa1420f779afef0dc7e99b
ad1vhaho <st10306086@rcconnect.edu.za>
2025-09-09 15:19:11 +0200

    Add project files.

---
+++ Prog2B_P1/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog2B_P1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
